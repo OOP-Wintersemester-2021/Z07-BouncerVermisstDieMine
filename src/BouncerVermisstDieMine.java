1e38415997a32d87bd19f8b39fab50c4b0f79648
Alexander Bazo <alexanderbazo@googlemail.com>
2021-11-02 16:52:21 +0100

    Prepare starter code

--- src/BouncerVermisstDieMine.java
+++ src/BouncerVermisstDieMine.java
@@ -2,6 +2,18 @@ import de.ur.mi.bouncer.apps.BouncerApp;
 import de.ur.mi.bouncer.apps.BouncerLauncher;
 
 
+/*
+ * Bouncer befindet sich am Eingang einer Mine. Vor ihm zweigen verschiedene
+ * Schächte ab. Bouncer soll den tiefsten Schaft finden und (nur) dessen Eingang
+ * mit einem blauen Feld markieren und am Ende der Mine stehen bleiben.
+ *
+ * Startbedingung: Bouncer steht am Eingang der Mine und blickt nach Osten. Mögliche
+ * Schächte beginnen direkt in der "Zeile" unter ihm. Auf dem ersten und dem letzten
+ * Feld können sich keine Schächte befinden.
+ *
+ * Gewünschtes Ergebnis: Bouncer steht auf dem letzten Feld der Mine und blickt
+ * nach Osten. Nur das Feld über dem tiefsten Schacht ist blau markiert.
+ */
 public class BouncerVermisstDieMine extends BouncerApp {
 
     @Override
